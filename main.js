e594ca242c9ca6971d7c18068769a1d39518cbce
Miki <paszkowski.mikolaj98@gmail.com>
2020-11-03 20:00:19 +0100

    closing

--- main.js
+++ main.js
@@ -3,14 +3,11 @@
 const URL = 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
 
 const getQuoteData = () => {
-    console.time('timer1');
     fetch(URL)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             document.querySelector('.quote-text').textContent = `${data.quote.quoteText}`;
             document.querySelector('.author').textContent = `~ ${data.quote.quoteAuthor}`;
-            console.timeEnd('timer1');
         })
         .catch(err => {
             console.log(err);
@@ -20,9 +17,13 @@ const getQuoteData = () => {
 const btnClose = () => {
 
     document.querySelector('.btn').addEventListener('click', (e) => {
-
-        document.body.style.opacity = '0';
-    })
+        document.querySelector('.btn').classList.add('btn-close');
+        
+        setTimeout(() => {
+            window.close();
+        }, 750);
+        
+    });
 }
 
 getQuoteData();
