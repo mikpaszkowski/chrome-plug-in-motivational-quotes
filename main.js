e5eca8c2061749144bb5e086ecdd11b8b2991cd5
Miki <paszkowski.mikolaj98@gmail.com>
2020-11-06 20:30:56 +0100

    function to get JSON from request

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const renderError = (msg) => {
     const errorMsg = `Something went wrong☹️ ${msg}. Try again!`;
     document.querySelector('.error').textContent = errorMsg;
     document.querySelector('.quote-text').style.display = 'none';
-}
+};
+
+const getJSONdata = (url, error = 'Something went wrong') => {
+    return fetch(url).then(response => {
+        if(!response.ok){
+            throw new Error(`Quote not found (${response.status})`);
+        }
+        return response.json();
+    });
+};
 
 const getQuoteData = () => {
-    fetch(URL)
-        .then(response => response.json())
+    getJSONdata(URL, 'Quote not found')
         .then(data => {
+            console.log(data);
             document.querySelector('.quote-text').textContent = `${data.quote.quoteText}`;
             document.querySelector('.author').textContent = `~ ${data.quote.quoteAuthor}`;
             document.querySelector('.quote-text').style.opacity = '1';
@@ -25,14 +34,13 @@ const getQuoteData = () => {
 const btnClose = () => {
 
     document.querySelector('.btn').addEventListener('click', (e) => {
-        document.querySelector('.btn').classList.add('btn-close');
-        
-        setTimeout(() => {
-            window.close();
-        }, 750);
+    document.querySelector('.btn').classList.add('btn-close');
+    setTimeout(() => {
+        window.close();
+    }, 750);
         
     });
-}
+};
 
 getQuoteData();
 btnClose();
