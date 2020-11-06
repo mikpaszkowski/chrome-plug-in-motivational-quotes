fc0fe5a95939ba6ea3e0d86d9fc8a828aa593dfd
Miki <paszkowski.mikolaj98@gmail.com>
2020-11-06 20:11:31 +0100

    error handling

--- main.js
+++ main.js
@@ -2,15 +2,23 @@
 
 const URL = 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
 
+const renderError = (msg) => {
+    const errorMsg = `Something went wrong☹️ ${msg}. Try again!`;
+    document.querySelector('.error').textContent = errorMsg;
+    document.querySelector('.quote-text').style.display = 'none';
+}
+
 const getQuoteData = () => {
     fetch(URL)
         .then(response => response.json())
         .then(data => {
             document.querySelector('.quote-text').textContent = `${data.quote.quoteText}`;
             document.querySelector('.author').textContent = `~ ${data.quote.quoteAuthor}`;
+            document.querySelector('.quote-text').style.opacity = '1';
         })
         .catch(err => {
             console.log(err);
+            renderError(err);
         })
 };
 
